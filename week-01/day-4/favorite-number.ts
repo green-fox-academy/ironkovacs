d973ef8304cc0bd52b2aa70680ee8ec6af9248a8
Ironkovacs <ironkovacs@gmail.com>
2018-02-15 13:13:17 +0100

    My favorite number is

---
+++ week-01/day-4/favorite-number.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Store your favorite number in a variable (as a number)
+let favoriteNumber: number = 42
+
+
+// And print it like this: 'My favorite number is: 8'
+console.log ("My favorite number is: " + favoriteNumber + ".");
