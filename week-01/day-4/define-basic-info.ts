8575da911dd5145e859fe87741ec7f706949b5d9
Ironkovacs <ironkovacs@gmail.com>
2018-02-15 14:08:42 +0100

    define basic info

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+let yourName: string = "Áron Kovács";
+// Your age as an integer
+let yourAge = 28;
+// Your height in meters as a float
+let yourHeight: number = 188;
+// Whether you are married or not as a boolean
+let married: boolean = false;
