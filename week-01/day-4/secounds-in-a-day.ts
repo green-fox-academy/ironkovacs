bd660939154d148b2764b36351cf5d31a1344231
Ironkovacs <ironkovacs@gmail.com>
2018-02-15 15:01:58 +0100

    seconds in a day_edit

--- week-01/day-4/secounds-in-a-day.ts
+++ week-01/day-4/secounds-in-a-day.ts
@@ -1,10 +1,10 @@
 'use strict';
 
-let currentHours: number = 14;
-let currentMinutes: number = 34;
-let currentSeconds: number = 42;
+let currentHours: number = 23;
+let currentMinutes: number = 59;
+let currentSeconds: number = 59;
 
 // Write a program that prints the remaining seconds (as an integer) from a
 // day if the current time is represented by these variables
-let remainingSecounds: number = ((currentHours * 60 + currentMinutes) * 60 + currentSeconds);
+let remainingSecounds: number = (24*60*60)-((currentHours * 60 + currentMinutes) * 60 + currentSeconds);
 console.log ("Remaining secound of the day is: " + `${remainingSecounds}`);
