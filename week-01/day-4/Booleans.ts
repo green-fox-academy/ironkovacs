afaac9d1f49a19aa204e5cd1f2143ce47b40e6f8
Ironkovacs <ironkovacs@gmail.com>
2018-02-15 11:38:15 +0100

    Booleans and strings

---
+++ week-01/day-4/Booleans.ts
@@ -0,0 +1,33 @@
+// true, false
+console.log(true); // Prints true
+console.log(false); // Prints false
+
+// Boolean Operators
+// Negation
+console.log(!true); // Prints false
+console.log(!false); // Prints true
+
+// And
+console.log(true && true); // Prints true
+console.log(true && false); // Prints false
+console.log(false && true); // Prints false
+console.log(false && false); // Prints false
+
+// Or
+console.log(true || true); // Prints true
+console.log(true || false); // Prints true
+console.log(false || true); // Prints true
+console.log(false || false); // Prints false
+
+// String and special characters
+console.log('banana'); // Prints banana
+console.log('don\'t'); // Prints don't
+console.log('"Everything is real." - Me');
+// Prints "Everything is real." - Me
+
+// String Operators
+// Concatenation
+console.log('fog' + 'kefe'); // Prints toothbrush
+
+// Concat string with number
+console.log('My favorite number is: ' + 42); // Prints My favorite number is: 8
