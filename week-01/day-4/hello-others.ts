95593c31bd4e1f657f1c8262a4d531c095558de7
Ironkovacs <ironkovacs@gmail.com>
2018-02-15 11:18:55 +0100

    hello others

---
+++ week-01/day-4/hello-others.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+console.log("Hello, Balázs!");
+console.log("Hello, Sára!");
+console.log("Hello, Gergő!")
