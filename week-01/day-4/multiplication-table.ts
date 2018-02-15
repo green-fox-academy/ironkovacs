b99021780abd24f76b8da0a0edb53bc814dda70f
Ironkovacs <ironkovacs@gmail.com>
2018-02-15 16:06:53 +0100

    multiplication table

---
+++ week-01/day-4/multiplication-table.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+let number: number = 2;
+let j: number = 0;
+for (let i: number = 0; i <= 10; i++) {
+    j = (number * i);
+    console.log(`${number}` + " x " + `${i}` + " = " + `${j}`); }
