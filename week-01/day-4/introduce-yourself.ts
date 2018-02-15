8516d555e843f5afb7e3e378f570121d82acb6fa
Ironkovacs <ironkovacs@gmail.com>
2018-02-15 11:43:38 +0100

    introduce-yourself

---
+++ week-01/day-4/introduce-yourself.ts
@@ -0,0 +1,12 @@
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+console.log ("Áron Kovács" + "\n" + "29" + "\n" + "1.88");
