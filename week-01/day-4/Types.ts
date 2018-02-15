65a696b6db3cd4ba96b03a568b390da721493f6f
Ironkovacs <ironkovacs@gmail.com>
2018-02-15 11:22:19 +0100

    types

---
+++ week-01/day-4/Types.ts
@@ -0,0 +1,11 @@
+// Prints a string to the terminal window
+console.log('The Great Answer Is:');
+
+// Prints an integer to the terminal window (MEANING OF THE UNIVERSE?!)
+console.log(42);
+
+// Prints a floating point number to the terminal window
+console.log(1.23456789);
+
+// Prints a boolean to the terminal window
+console.log(false);
