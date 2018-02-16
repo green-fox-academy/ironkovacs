3ef49d33d50082d338438dc1511554dbdf4e2522
Ironkovacs <ironkovacs@gmail.com>
2018-02-16 14:04:56 +0100

    Diamond is  working

--- week-01/day-5/draw-diamond.ts
+++ week-01/day-5/draw-diamond.ts
@@ -1,34 +1,36 @@
 'use strict';
-
+//INPUT
 let lineCount: number = 10;
-
-let brush = "*";
-let space = " ";
-let aend
-let bend
-//felső fél
-for (let a: number = 0; a < lineCount; a++) {
-    let draw = '';
-    for (let b: number = 1; b < lineCount-a; b++) {
+let brush: string = "*"; // Paintbrush
+//Calculation and stuffZ
+let lineCountTop: number = Math.ceil(lineCount/2);
+let lineCountBot: number = lineCountTop -1;
+let space: string = " ";
+let draw: string = '';
+//TOP HALF
+for (let a: number = 0; a < lineCountTop; a++) {
+    draw = '';
+    for (let b: number = 1; b < lineCountTop-a; b++) {
       draw = draw + space;
     }
     for (let c: number = 1; c <= (2*a+1); c++) {
         draw = draw + brush ;
     }
-
     console.log (draw);
-       }
-    
-
-// alsó fél
+}
+//Rubbish for even lines
+   if (lineCount % 2 ==0){
+        console.log (draw);
+    }
+//BOTTOM HALF
 space = " ";
 brush = "*";
-for (let a: number = lineCount - 1; a > 0; a--) {
-    let draw = ' ';
-    for (let b: number = lineCount - a; b > 1; b--) {
+for (let a: number = lineCountBot; a > 0; a--) {
+    draw = ' ';
+    for (let b: number = lineCountBot - a; b > 0; b--) {
         draw = space + draw;}
     for (let c: number = (2*a-1); c >= 1; c--) {
         draw = draw + brush;
     }
     console.log (draw);
-       }
+}
