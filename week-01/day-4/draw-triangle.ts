27aed754387b3e8dd5a1eceab56b26e7507f768c
Ironkovacs <ironkovacs@gmail.com>
2018-02-16 10:40:31 +0100

    frakking pyramid

---
+++ week-01/day-4/draw-triangle.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let lineCount: number = 4;
+let brush = "*";
+let star: string ='';
+for (let i: number = 0; i<= lineCount; i++ ){
+    console.log(brush)
+    brush += "*"
+}
---
+++ week-01/day-5/draw-pyramid.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+let lineCount: number = 10;
+
+let brush = "*";
+let space = " ";
+for (let i = 0; i < lineCount; i++) {
+    let draw = '';
+    for (let j = 1; j < lineCount-i; j++) {
+      draw = draw + space;
+    }
+    for (var k = 1; k <= (2*i+1); k++) {
+        draw = draw + brush ;
+    }
+    console.log(draw);
+  }
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
