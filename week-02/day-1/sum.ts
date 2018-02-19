668483a2aaa5f196246408da830b0f2e62103055
Ironkovacs <ironkovacs@gmail.com>
2018-02-19 12:19:56 +0100

    sum 5 elements

---
+++ week-02/day-1/sum.ts
@@ -0,0 +1,14 @@
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+
+let s1: number = 3;
+let s2: number = 4;
+let s3: number = 5;
+let s4: number = 3;
+let s5: number = 3;
+
+function sum(a , b , c , d , e){
+    return (a + b + c + d + e);
+} 
+
+console.log(sum(s1, s2, s3, s4, s5));
