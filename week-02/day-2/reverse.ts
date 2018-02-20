e1f74a5a1ad79cbd21dff013b07fb8d4a33619af
Ironkovacs <ironkovacs@gmail.com>
2018-02-20 13:49:31 +0100

    revese with multi solution

--- week-02/day-2/reverse.ts
+++ week-02/day-2/reverse.ts
@@ -8,12 +8,22 @@ function reverse(str: string){
   return str.split('').reverse().join('');
 }
 
-console.log(reverse(reversed));
+console.log('Result with split + reverse + join:' , reverse(reversed));
 
-function reverseWithFor(str: string) {
-  
-  for (let i: number = 0; i <= str.length; i++)
-    str[i] = str[str.length - i];
-  return str;
+function reverseWithForInc(str: string) {
+  let str2: string = '';
+  for (let i: number = 0; i < str.length; i++){
+        str2 = str2.concat(str[str.length - (i + 1)]);        
+  }
+  return str2;
 }
-console.log(reverseWithFor(reversed));
+console.log('Result with for, inc. :', reverseWithForInc(reversed));
+
+function reverseWithForDec(str: string) {
+  let str2: string = '';
+  for (let i: number = str.length; 0 < i; i--){
+        str2 = str2.concat(str[i-1]);        
+  }
+  return str2;
+}
+console.log('Result with for, dec.:', reverseWithForDec(reversed));
