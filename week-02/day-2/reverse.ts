1cfb3eedbd9608d1ade63b5fea5967e5c9e2f6ed
Ironkovacs <ironkovacs@gmail.com>
2018-02-20 12:33:07 +0100

    reverse with array

--- week-02/day-2/reverse.ts
+++ week-02/day-2/reverse.ts
@@ -3,13 +3,17 @@
 // Create a function that can reverse a string, which is passed as the parameter
 // Use it on this reversed string to check it!
 let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
-let reversedSrting: string = '1';
-
-function reverse(str: string){
- 
+let reversedStr: string = "";
+function reverse(str: string){ 
   return str.split('').reverse().join('');
 }
 
 console.log(reverse(reversed));
 
-export = reverse;
+function reverseWithFor(str: string) {
+  
+  for (let i: number = 0; i <= str.length; i++)
+    str[i] = str[str.length - i];
+  return str;
+}
+console.log(reverseWithFor(reversed));
