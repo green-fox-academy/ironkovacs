2247a899a11f4422079f1b75caf4af337e4829ad
Ironkovacs <ironkovacs@gmail.com>
2018-02-20 14:28:35 +0100

    takes-longer

---
+++ week-02/day-2/todo-print.ts
@@ -0,0 +1,18 @@
+
+'use strict';
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indention
+// Expected outpt:
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
+let todoText: string = ' - Buy milk\n';
+let todo0: string = 'My todo:\n';
+let todo1: string = todoText;
+let todo2: string = ' - Download games\n';
+let todo3: string = '   - Diablo\n';
+
+todoText.concat(todo0, todo1, todo2, todo3);
+console.log(todoText);
