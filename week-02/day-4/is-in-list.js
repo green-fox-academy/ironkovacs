b2dd39209e48e7ca5a350ca40a3671bdc19b6c09
Ironkovacs <ironkovacs@gmail.com>
2018-02-22 14:33:12 +0100

    includes almost working in .ts

--- week-02/day-4/is-in-list.js
+++ week-02/day-4/is-in-list.js
@@ -5,18 +5,9 @@
 let listOfNumbers = [2, 4, 6, 8, 10, 12, 14, 16]; //.TSError: ⨯ Unable to compile TypeScript is-in-list.ts (11,15): Property 'includes' does not exist on type 'any[]'. (2339),
 let isOnList = [4, 8, 12, 16]
 
-function includesTs(a: number[], n:any){ //would be nice with one value :(
-  if (a.indexOf(n) != -1) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/*runs in .js only !!!
 function checkNums(list1, list2) {
-  let checkList :any = [];
-  for (let i: number = 0; i < list1.length; i++) {
+  let checkList = [];
+  for (let i = 0; i < list1.length; i++) {
     if (list1.includes(list2[i]) == true) {
       checkList.push(1);
     } else {
@@ -29,24 +20,6 @@ function checkNums(list1, list2) {
     return true;
   }
 }
-console.log(checkNums(listOfNumbers, isOnList));*/
-
-// hope to .ts
-function checkNums(list1: number[], list2: number[]) {
-  let checkList = [1];
-  for (let i: number = 0; i < list1.length; i++) {
-    if (includesTs(list1, list2[i]) == true) {
-      checkList.push(1);
-    } else {
-      checkList.push(0);
-    }
-  }
-  if (includesTs(checkList, 0)) {
-    return false;
-  } else {
-    return true;
-  }
-}
 console.log(checkNums(listOfNumbers, isOnList));
 
-export = checkNums;
+//export = checkNums;
