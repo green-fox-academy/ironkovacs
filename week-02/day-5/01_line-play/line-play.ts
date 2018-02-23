2bbbed8007d7ed4d4ae4e0dd07abb502eafb2c46
Ironkovacs <ironkovacs@gmail.com>
2018-02-23 10:55:15 +0100

    line play

---
+++ week-02/day-5/01_line-play/line-play.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+let amountOfLines: number = 20;
+let amountOfTiles: number = 4;
+
+function quadrants(n: number) {
+  let cHeight: number = canvas.height / n;
+  let cWidth: number = canvas.width / n;
+  return [cHeight, cWidth];
+}
+
+function arcLines(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);;
+  ctx.stroke();
+}
+function arcPairs(shiftX, shiftY) {
+  let h: number = quadrants(amountOfTiles)[0];
+  let w: number = quadrants(amountOfTiles)[1];
+  for (let i = 0; i < amountOfLines; i++) {
+    let x = i * w / (amountOfLines - 1);
+    let y = i * h / (amountOfLines - 1);
+    arcLines(0 + shiftX, y + shiftY, x + shiftX, h + shiftY);
+    arcLines(x + shiftX, 0 + shiftY, w + shiftX, y + shiftY);
+  }
+}
+for (let i = 0; i < amountOfTiles; i++) {
+  for (let j = 0; j < amountOfTiles; j++) {
+    arcPairs(i * quadrants(amountOfTiles)[1], j * quadrants(amountOfTiles)[0]);
+  }
+}
+
+
+
+
