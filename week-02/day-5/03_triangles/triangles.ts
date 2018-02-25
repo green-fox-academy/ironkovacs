07cc582a72456e1e53efb728f9473d0b2b33aa69
Áron Kovács <ironkovacs@gmail.com>
2018-02-25 18:01:31 +0100

    triangles

--- week-02/day-5/03_triangles/triangles.ts
+++ week-02/day-5/03_triangles/triangles.ts
@@ -4,23 +4,24 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
-let base = 100;
-let h = base * (((3) ** (1 / 2) / 2));
-let startX = canvas.width / 2 - base / 2;
-let startY = 0;
-function triangle(a) {
+let lineCount: number = 10;
+let base: number = 100;
+let h: number = base * (((3) ** (1 / 2) / 2));
+let startX: number = canvas.width / 2 - base / 2;
+let startY: number = 0;
+function triangle(a, shiftX, shiftY) {
   ctx.beginPath();
-  ctx.moveTo(startX + a / 2, startY);
-  ctx.lineTo(startX + a, startY + h);
-  ctx.lineTo(startX, startY + h);
-  ctx.lineTo(startX + a / 2, startY);
+  ctx.moveTo(startX + a + shiftX / 2, startY + shiftY);
+  ctx.lineTo(startX + a + shiftX, startY + h + shiftY);
+  ctx.lineTo(startX + shiftX, startY + h + shiftY);
+  ctx.lineTo(startX + shiftX + a / 2, startY + shiftY);
   ctx.stroke();
 }
 
-for (let i = 0, len = iterable.length; i < len; i++) {
-  
-}  
+for (let i: number = 0; i < lineCount; i++) {
+  for (let j: number = 0; i < lineCount; j++) {
+    let x = base * i;
+    let y = h * j;
+    triangle(base, x, y);
+  }
 }
-  
-}
-triangle(base);
