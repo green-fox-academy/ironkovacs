09e64bff94fad1fe022b15116a5671d7e0ef19aa
Ironkovacs <ironkovacs@gmail.com>
2018-02-27 13:31:30 +0100

    fixed for windows

--- week-03/day-2/count-lines.ts
+++ week-03/day-2/count-lines.ts
@@ -5,11 +5,12 @@
 // should not raise any error.
 
 const fs = require('fs');
-let myFileName: string = 'my-fiasdasdasdle.txt'
+let myFileName: string = 'my-file.txt'
+
 function lineCount(filename) {
   let text: string = fs.readFileSync(filename, 'utf-8');
-  let textA: string[] = text.split('\n');
-  return console.log(textA.length);
+  let textA: string[] = text.split('\r\n');
+  return console.log(textA.length, textA);
 }
 try {
   lineCount(myFileName);
