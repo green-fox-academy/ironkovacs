debb18fc4d0779dc46af2ac3af3543d76dabcac3
Ironkovacs <ironkovacs@gmail.com>
2018-02-27 13:15:05 +0100

    That's just how I roll

---
+++ week-03/day-2/count-lines.ts
@@ -0,0 +1,22 @@
+'use-strict';
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+const fs = require('fs');
+let myFileName = 'my-fiasdasdasdle.txt'
+function lineCount(filename) {
+  let text: string = fs.readFileSync(filename  , 'utf-8');
+  let textA: string [] = text.split('\n');
+  return console.log(textA.length);
+}
+try {
+  lineCount(myFileName);
+}
+catch(err) {
+ console.log(0)
+}
+
+
+
