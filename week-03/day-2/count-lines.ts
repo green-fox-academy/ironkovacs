35c5d4a45afe6c31ee6e0896f6303eb96655a546
Ironkovacs <ironkovacs@gmail.com>
2018-02-27 13:42:18 +0100

    use strict

--- week-03/day-2/count-lines.ts
+++ week-03/day-2/count-lines.ts
@@ -1,4 +1,4 @@
-'use-strict';
+'use strict';
 // Write a function that takes a filename as string,
 // then returns the number of lines the file contains.
 // It should return zero if it can't open the file, and
---
+++ week-03/day-2/write-single-line.ts
@@ -0,0 +1,10 @@
+'use strict';
+// Open a file called 'my-file.txt'
+// Write your name in it as a single line
+// If the program is unable to write the file,
+// then it should print an error message like: 'Unable to write file: my-file.txt'
+
+const fs = require('fs');
+
+let fileContent = 'I can write';
+fs.writeFileSync('message.txt', fileContent);
