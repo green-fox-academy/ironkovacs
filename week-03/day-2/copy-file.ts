ad9093ab5dee5470b39cc32ffa3ba64928db1747
Ironkovacs <ironkovacs@gmail.com>
2018-02-27 15:27:58 +0100

    LOTS of changes. period

--- week-03/day-2/copy-file.ts
+++ week-03/day-2/copy-file.ts
@@ -2,3 +2,15 @@
 // Write a function that copies a file to an other
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
+
+const fs = require('fs');
+
+function copies(file1: string, file2: string) {
+  try {
+    fs.writeFileSync(file1, (fs.readFileSync(file2, 'utf-8')));
+    return true;
+  } catch (err) {
+    return false
+  }
+}
+console.log(copies('tx1.txt', 'tx3.txt'));
