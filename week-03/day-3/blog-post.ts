f9e60c428b8572f5d2136ae23c653af6964967ca
Ironkovacs <ironkovacs@gmail.com>
2018-02-28 11:56:47 +0100

    Added a banner to the default admin page. Please have mercy on me =(

---
+++ week-03/day-3/blog-post.ts
@@ -0,0 +1,32 @@
+'use strict';
+// Create a BlogPost class that has
+// an authorName
+// a title
+// a text
+// a publicationDate
+// Create a few blog post objects:
+// "Lorem Ipsum" titled by John Doe posted at "2000.05.04."
+// Lorem ipsum dolor sit amet.
+// "Wait but why" titled by Tim Urban posted at "2010.10.10."
+// A popular long-form, stick-figure-illustrated blog about almost everything.
+// "One Engineer Is Trying to Get IBM to Reckon With Trump" titled by William Turton at "2017.03.28."
+// Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.
+
+class BlogPost {
+  public authorName: string;
+  public title: string;
+  public publicationDate: string;
+
+
+  constructor(authorName, title, text, publicationDate) {
+    BlogPost.counter++;
+    this.authorName = authorName;
+    this.title = title;
+    this.publicationDate = publicationDate;
+
+  }
+}
+
+let blogPost_0001 = new BlogPost('John Doe', 'Lorem ipsum', 'Lorem ipsum dolor sit amet.' '2000.05.04');
+let blogPost_0002 = new BlogPost('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10')
+let blogPost_0003 = new BlogPost('William Turton', 'One Engineer Is Trying to Get IBM to Reckon With Trump', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing', '2017.03.28')
