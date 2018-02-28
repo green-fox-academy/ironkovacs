b2729ecdffd9d96c281c3dc4a22a139e13b2b977
Ironkovacs <ironkovacs@gmail.com>
2018-02-28 11:57:17 +0100

    added super-widget 2.0.

--- week-03/day-3/blog-post.ts
+++ week-03/day-3/blog-post.ts
@@ -27,6 +27,6 @@ class BlogPost {
   }
 }
 
-let blogPost_0001 = new BlogPost('John Doe', 'Lorem ipsum', 'Lorem ipsum dolor sit amet.' '2000.05.04');
+let blogPost_0001 = new BlogPost('John Doe', 'Lorem ipsum', 'Lorem ipsum dolor sit amet.', '2000.05.04');
 let blogPost_0002 = new BlogPost('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10')
 let blogPost_0003 = new BlogPost('William Turton', 'One Engineer Is Trying to Get IBM to Reckon With Trump', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing', '2017.03.28')
