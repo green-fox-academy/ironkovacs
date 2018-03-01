0f4ebc2d145c8481c49acb6527101135811493ab
Ironkovacs <ironkovacs@gmail.com>
2018-03-01 13:03:55 +0100

    mutant bunnies

--- week-03/day-4/bunny2.ts
+++ week-03/day-4/bunny2.ts
@@ -3,19 +3,19 @@
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 // have 3 ears, because they each have a raised foot. Recursively return the
 // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
-function bunny(n) { 
-    let ears: number = 2;
-    if (n < 1) {
-      return 0;
-    } else if(n % 2 != 0){
-      n -= 1;
-      ears += bunny(n);
-    }else {
-      n -= 1;
-      ears += bunny(n)+1;
-    }
-    return ears;
-  
+function bunny(n) {
+  let ears: number = 2;
+  if (n < 1) {
+    return 0;
+  } else if (n % 2 != 0) {
+    n -= 1;
+    ears += bunny(n);
+  } else {
+    n -= 1;
+    ears += bunny(n) + 1;
+  }
+  return ears;
+
 }
 
 console.log(bunny(5))
