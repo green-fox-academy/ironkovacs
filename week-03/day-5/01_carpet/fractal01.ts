7c61453b23f3e60e5e3d069ca1e4badb70487019
Ironkovacs <ironkovacs@gmail.com>
2018-03-02 13:51:34 +0100

    colorful stuffs animated

--- week-03/day-5/01_carpet/fractal01.ts
+++ week-03/day-5/01_carpet/fractal01.ts
@@ -5,40 +5,42 @@ const ctx = canvas.getContext('2d');
 
 let size: number = canvas.width / (Math.floor((Math.random() * 2) + 1))
 
-let middleX: number = canvas.width / 2;
-let middleY: number = canvas.height / 2;
-let lineWidth: number = 10;
+let shift: number = canvas.width / 2;
+let lineWidth: number = 25;
 
 let h: number = 0;
-let s: number = 0;
-let l: number = 50;
+let s: number = 100;
+let l: number = 100;
+let a: number = 100;
 function carpet(x: number, y: number, size: number, b: number) {
 
-  if (size >= 3) {
+  if (size >= 2) {
 
     ctx.lineWidth = b;
-    ctx.strokeStyle = 'hsl(' + `${h}` + ', ' + `${s}` + '%, ' + `${l}` + '%)';
-    ctx.strokeRect(x + middleX - size / 2, y + middleY - size / 2, size, size);
+    ctx.strokeStyle = 'hsl(' + `${h}` + ', ' + `${s}` + '%, ' + `${l}` + '%, ' + `${a}` + '%)';
+    ctx.strokeRect(x + shift - size / 2, y + shift - size / 2, size, size);
 
     b /= 2
     size /= 2;
+    l = 50;
+    setTimeout(function () {
+      h = 60;
+      s = s * 0.99;
+      carpet(x + size, y + size, size, b);
+      h = 120;
+      s = s * 0.99;
+      carpet(x + size, y - size, size, b);
+      h = 200;
+      s = s * 0.99;
+      carpet(x - size, y + size, size, b);
+      h = 0;
+      s = s * 0.99;
+      carpet(x - size, y - size, size, b);
+    },
+      500);
 
-    h = 90;
-    s += 10;
-    carpet(x + size, y + size, size, b);
-    h = 180;
-    s += 10;
-    carpet(x + size, y - size, size, b);
-    h = 270;
-    s += 10;
-    carpet(x - size, y + size, size, b);
-    h = 0;
-    s += 10;
-    carpet(x - size, y - size, size, b);
+
+
   }
-
-
-
 }
-
 carpet(0, 0, size, lineWidth)
