d4ffb2f569a0c5220508a819caa38b77c877a16f
Ironkovacs <ironkovacs@gmail.com>
2018-03-02 13:15:36 +0100

    colorful grid

--- week-03/day-5/01_carpet/fractal01.ts
+++ week-03/day-5/01_carpet/fractal01.ts
@@ -8,28 +8,33 @@ let size: number = canvas.width / (Math.floor((Math.random() * 2) + 1))
 let middleX: number = canvas.width / 2;
 let middleY: number = canvas.height / 2;
 let lineWidth: number = 10;
-//let c = 'hsl ('+`${h}`+`${s}`+`${l}`+')'
 
+let h: number = 0;
+let s: number = 0;
+let l: number = 50;
 function carpet(x: number, y: number, size: number, b: number) {
+
   if (size >= 3) {
-    let h: number = 0;
-    let s: number = 0;
-    let l: number = 0;
 
     ctx.lineWidth = b;
-    ctx.strokeStyle = 'hsl (' + `${h}` + `${s}` + `${l}` + ')'
+    ctx.strokeStyle = 'hsl(' + `${h}` + ', ' + `${s}` + '%, ' + `${l}` + '%)';
     ctx.strokeRect(x + middleX - size / 2, y + middleY - size / 2, size, size);
-
 
     b /= 2
     size /= 2;
 
-
+    h = 90;
+    s += 10;
     carpet(x + size, y + size, size, b);
+    h = 180;
+    s += 10;
     carpet(x + size, y - size, size, b);
+    h = 270;
+    s += 10;
     carpet(x - size, y + size, size, b);
+    h = 0;
+    s += 10;
     carpet(x - size, y - size, size, b);
-
   }
 
 
