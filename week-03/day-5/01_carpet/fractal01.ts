84b13fcb296541463c7d3c7371cbf4e7ada9c128
Ironkovacs <ironkovacs@gmail.com>
2018-03-02 11:16:37 +0100

    carpet v1.0

---
+++ week-03/day-5/01_carpet/fractal01.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+let size: number = canvas.width / (Math.floor((Math.random() * 2) + 1))
+
+let middleX: number = canvas.width / 2;
+let middleY: number = canvas.height / 2;
+let lineWidth: number = 10;
+//let c = 'hsl ('+`${h}`+`${s}`+`${l}`+')'
+
+function carpet(x: number, y: number, size: number, b: number) {
+  if (size >= 3) {
+    let h: number = 0;
+    let s: number = 0;
+    let l: number = 0;
+
+    ctx.lineWidth = b;
+    ctx.strokeStyle = 'hsl (' + `${h}` + `${s}` + `${l}` + ')'
+    ctx.strokeRect(x + middleX - size / 2, y + middleY - size / 2, size, size);
+
+
+    b /= 2
+    size /= 2;
+
+
+    carpet(x + size, y + size, size, b);
+    carpet(x + size, y - size, size, b);
+    carpet(x - size, y + size, size, b);
+    carpet(x - size, y - size, size, b);
+
+  }
+
+
+
+}
+
+carpet(0, 0, size, lineWidth)
