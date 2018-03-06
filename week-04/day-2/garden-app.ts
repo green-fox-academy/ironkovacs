41b9eaa3ebbeebd144ffd5876df277a796d26e43
Ironkovacs <ironkovacs@gmail.com>
2018-03-06 16:14:05 +0100

    garden

--- week-04/day-2/garden-app.ts
+++ week-04/day-2/garden-app.ts
@@ -1,20 +1,52 @@
 'use strict';
 
-class Garden{
+class Garden {
+  flowers: Plant[] = [];
+  trees: Plant[] = [];
+
+  addNewPlant(type) {
+    this.t
+  }
+
+  watering() {
+
+  }
+  getWateringStatus() {
+
+  }
+  constructor(flowers: Plant[], trees: Plant[], type: string) {
+    this.flowers = flowers;
+    this.trees = trees;
+    
+
+  }
 
 }
 
-class Plant{
+class Plant {
+  type: string;
   color: string;
   waterAbsorb: number;
   waterStatus: number = 0;
 
-}
+    
+  constructor(color: string, waterAbsorb: number, waterStatus: number = 0, type: string){
+  }
 
-class Tree{
-
-}
-
-class Fower{
 
 }
+
+class Tree extends Plant {
+
+  constructor(color?: string, waterAbsorb: number = 40, waterStatus: number = 0, type: string = "tree"){
+    super(color, waterAbsorb, waterStatus, type);
+  }
+}
+
+class Fower extends Plant {
+  constructor(color: string, waterAbsorb: number = 70, waterStatus: number = 0, type: string = "flower"){
+    super(color, waterAbsorb, waterStatus, type);
+  }
+
+}
+
