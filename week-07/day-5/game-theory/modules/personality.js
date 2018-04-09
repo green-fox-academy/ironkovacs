74549b43d92428ebd3eb7f3706ac38936436243a
Áron Kovács <ironkovacs@gmail.com>
2018-04-10 01:45:31 +0200

    game theory

---
+++ week-07/day-5/game-theory/modules/personality.js
@@ -0,0 +1,33 @@
+
+function copycat() {
+  if (opponentResult.length <= 0 || playerStrategy[playerStrategy.length - 1] === 'coop') {
+    opponentCoop();
+  }
+  if (playerStrategy[playerStrategy.length - 1] === 'cheat') {
+    opponentCheat();
+  }
+}
+
+function mrCheat() {
+  opponentCheat();
+}
+
+function msCoop() {
+  opponentCoop();
+}
+
+function avenger() {
+  if (playerStrategy.indexOf('cheat') !== -1) {
+    opponentCoop();
+  } else {
+    opponentCheat();
+  }
+}
+
+function analytic() {
+  if (opponentStrategy.length > 0) {
+    opponentStrategy = [];
+  }
+}
+
+export { copycat, mrCheat, msCoop, avenger, analytic };
