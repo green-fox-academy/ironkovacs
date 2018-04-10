4256f8e63905af94757edcad0ce752801d653070
Áron Kovács <ironkovacs@gmail.com>
2018-04-10 09:41:38 +0200

    monday

--- week-08/day-1/Poker/calculator.js
+++ week-08/day-1/Poker/calculator.js
@@ -3,16 +3,70 @@ module.exports = (blackHand, whiteHand) => {
   let winnerCard;
   const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A'];
 
-  if (values.indexOf(blackHand[4][0]) > values.indexOf(whiteHand[4][0])) {
-    winner = 'Black';
-    winnerCard = blackHand[4][0];
-  } else {
-    winner = 'White';
-    winnerCard = whiteHand[4][0];
+  let blackPair = false;
+  let whitePair = false;
+
+  let blackDrill = false;
+  let whiteDrill = false;
+
+  blackHand.forEach((e, i) => {
+    if (blackHand[i + 1] !== undefined) {
+      if (blackHand[i][0] === blackHand[i + 1][0] === blackHand[i + 1][0]) {
+        blackdrill = true;
+        winnerCard = blackHand[i][0];
+      }
+    }
+  });
+
+  blackHand.forEach((e, i) => {
+    if (blackHand[i + 1] !== undefined) {
+      if (blackHand[i][0] === blackHand[i + 1][0] === blackHand[i + 1][0]) {
+        blackdrill = true;
+        winnerCard = blackHand[i][0];
+      }
+    }
+  });
+
+  blackHand.forEach((e, i) => {
+    if (blackHand[i + 1] === undefined) {
+    } else if (blackHand[i][0] === blackHand[i + 1][0]) {
+      blackPair = true;
+      winnerCard = blackHand[i][0];
+    }
+  });
+
+  whiteHand.forEach((e, i) => {
+    if (whiteHand[i + 1] === undefined) {
+    } else if (whiteHand[i][0] === whiteHand[i + 1][0]) {
+      whitePair = true;
+      winnerCard = whiteHand[i][0];
+    }
+  });
+
+  if (blackDrill === true || whiteDrill === true){
+    if (blackDrill === true){
+      return winner = 'Black';
+    }else { 
+      return winner = 'White';
+    }
   }
-  if (winnerCard === 'A'){
-    winnerCard = 'Ace';
-  }  
-  console.log(`${winner} Wins! - (High card: ${winnerCard})`)
-  return `${winner} wins! - (High card: ${winnerCard})`;
+  if (whitePair === true || blackPair === true) {
+    if (whitePair) {
+      return `White wins! - (Pair: ${winnerCard})`;
+    } else {
+      return `Black wins! - (Pair: ${winnerCard})`;
+    }
+  } else {
+    if (values.indexOf(blackHand[4][0]) > values.indexOf(whiteHand[4][0])) {
+      winner = 'Black';
+      winnerCard = blackHand[4][0];
+    } else {
+      winner = 'White';
+      winnerCard = whiteHand[4][0];
+    }
+    if (winnerCard === 'A') {
+      winnerCard = 'Ace';
+    }
+    return `${winner} wins! - (High card: ${winnerCard})`;
+  }
 }
--- week-08/day-1/Poker/test.js
+++ week-08/day-1/Poker/test.js
@@ -2,31 +2,50 @@ const test = require('tape');
 const pokerCalculator = require('./calculator');
 
 test('High card: Ace', (t) => {
-  const expectedResult = "White wins! - (High card: Ace)"
+  const expectedResult = "White wins! - (High card: Ace)";
   const blackHand = ['2H', '3D', '5S', '9C', 'KD'];
   const whiteHand = ['2C', '3H', '4S', '8C', 'AH'];
-
   t.equal(pokerCalculator(blackHand, whiteHand), expectedResult);
   t.end();
 });
 
 
 test('High card: Ace', (t) => {
-  const expectedResult = 'Black wins! - (High card: Ace)'
-
+  const expectedResult = 'Black wins! - (High card: Ace)';
   const blackHand = ['2H', '3D', '5S', '9C', 'AD'];
   const whiteHand = ['2C', '3H', '4S', '8C', 'KD'];
-
   t.equal(pokerCalculator(blackHand, whiteHand), expectedResult);
   t.end();
 });
 
 test('High card: T', (t) => {
   const expectedResult = 'Black wins! - (High card: T)';
-
   const blackHand = ['2H', '3D', '5S', '6C', 'TH'];
   const whiteHand = ['2C', '3H', '4S', '8C', '9D'];
+  t.equal(pokerCalculator(blackHand, whiteHand), expectedResult);
+  t.end();
+});
 
+test('Pair (8)', (t) => {
+  const expectedResult = 'White wins! - (Pair: 8)';
+  const blackHand = ['2H', '3D', '5S', '9C', 'KD'];
+  const whiteHand = ['2C', '3H', '4S', '8C', '8S'];
+  t.equal(pokerCalculator(blackHand, whiteHand), expectedResult);
+  t.end();
+});
+
+test('Pair (5)', (t) => {
+  const expectedResult = 'Black wins! - (Pair: 5)';
+  const blackHand = ['2H', '5D', '5S', 'KC', 'AH'];
+  const whiteHand = ['2C', '3H', '4S', '8C', 'KD'];
+  t.equal(pokerCalculator(blackHand, whiteHand), expectedResult);
+  t.end();
+});
+
+test('Drill (K)', (t) => {
+  const expectedResult = 'Black wins! - (Drill: K)';
+  const blackHand = ['2H', '2D', '2S', 'TC', 'KD'];
+  const whiteHand = ['2C', '3H', '4S', '8C', '8S'];
   t.equal(pokerCalculator(blackHand, whiteHand), expectedResult);
   t.end();
 });
