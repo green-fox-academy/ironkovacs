bfbadf283c7057adb08da9fd7e0025ea6c4c080d
Áron Kovács <ironkovacs@gmail.com>
2018-04-11 18:57:00 +0200

    asd

--- week-08/balazskaxdCA/shop/src/index.js
+++ week-08/balazskaxdCA/shop/src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-  res.send("hello W");
+  res.render('index', {
+    title: 'Dashboard',
+  });
 });
 
-app.listen(PORT, () =>{
-  console.log(`app is runing on ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`app is runing on ${PORT}`); //eslint-disable-line
 });
