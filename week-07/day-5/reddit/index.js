a9d800d4bbdc62f864d6a7bc09c564a1e3ddb546
Áron Kovács <ironkovacs@gmail.com>
2018-04-12 18:07:54 +0200

    reddit

--- week-07/day-5/reddit/index.js
+++ week-07/day-5/reddit/index.js
@@ -1,36 +1,75 @@
-'use strict';
 const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+
 const app = express();
 const PORT = 3000;
-const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '1234',
-
+  database: 'reddit',
+  port: 3306,
 });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use(express.json());
+
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Connected!');
+  console.log('Connected!'); //eslint-disable-line
 });
 
-app.get('/post', (req, res) => {
-  const post = req.query;
-  res.json({
-    posts: [
-      {
-        id: post.id,
-        title: post.title,
-        url: post.url,
-        timestamp: timestamp,
-        score: post.score,
-      },]
+app.get('/', (req, res) => {
+  let sql = 'SELECT * FROM reddit_post LIMIT 20;';
+  let queryInputs = [];
+  if (req.query.type !== undefined) {
+    sql = 'SELECT * FROM reddit_post WHERE type = ? LIMIT 20;';
+    queryInputs = [req.query.type];
+  }
+  connection.query(sql, queryInputs, (err, result) => {
+    if (err) {
+      console.log(err); //eslint-disable-line
+      res.sendStatus(500);
+    }
+    console.log(result);
+    res.render('home', {
+      title: 'JETit!',
+      history: result,
+    });
   });
 });
 
-app.post
+app.get('/newpost', (req, res) => {
+  res.render('post.ejs', {
+    title: 'JETit!',
+  });
+});
 
+app.post('/post', (req, res) => {
+  const sql = 'INSERT INTO reddit_post (title, url, score) VALUES (?, ?, ?);';
+  const post = req.body;
+  const queryInputs = [post.title, post.url, 0];
+
+  connection.query(sql, queryInputs, (err, result, fields) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log(result);
+      console.log(fields);
+      res.json({
+        id: 3,
+        title: post.title,
+        url: post.url,
+        score: post.score,
+      });
+    }
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`); //eslint-disable-line
 });
